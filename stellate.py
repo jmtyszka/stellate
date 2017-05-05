ed156fb6f3c293a10606f5206375cced8e1eb3e3
Mike Tyszka <jmt@caltech.edu>
2017-05-05 12:29:57 -0700

    Switch to 16-bit PNG stacks for input and output. Add rigid body coregistration at full resolution (skimage).

--- stellate.py
+++ stellate.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.5
 """
 Perform blind deconvolution and within-stack registration of astrophotos
 
 Usage
 ----
-stellate.py -i <RAW image directory> -o <Output directory>
+stellate.py -i <Raw image directory> -o <Output directory>
 stellate.py -h
 
 Authors
@@ -42,7 +42,7 @@ __version__ = '0.1.0'
 import os
 import sys
 import argparse
-import numpy as np
+from skimage import io
 
 
 def main():
@@ -69,44 +69,112 @@ def main():
         # Default to current directory
         raw_dir = os.path.realpath(os.getcwd())
 
-    print('Input directory : %s' % raw_dir)
-
     if args.outdir:
         out_dir = args.outdir
     else:
-        # Default to current directory
-        out_dir = os.path.realpath(os.getcwd())
+        # Default to current directory + '.proc'
+        out_dir = os.path.realpath(os.getcwd() + '.proc')
 
-    print('Output directory    : %s' % out_dir)
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir, exist_ok=True)
+
+    print('Input directory  : %s' % raw_dir)
+    print('Output directory : %s' % out_dir)
 
     # Load all RAW images from input directory
-    load_raw_stack(raw_dir)
+    stack_rgb = load_png_stack(raw_dir)
 
-    #
+    # Correct for inter-frame motion
+    stack_rgb_moco = register_stack(stack_rgb)
 
+    # Blind deconvolution
+    # stack_decon = blind_deconvolve(stack_rgb_moco)
+
+    # Save stack
+    save_stack(stack_rgb_moco, out_dir)
 
     # Clean exit
     sys.exit(0)
 
 
-def blind_deconvolve(stack):
+def register_stack(stack_rgb):
 
-    return []
+    from skimage.feature import register_translation
+    from skimage.transform import SimilarityTransform, warp
+    from skimage.color import rgb2gray
+
+    ref_gray = rgb2gray(stack_rgb[0])
+
+    stack_rgb_moco = list()
+
+    for img_rgb in stack_rgb:
+
+        img_gray = rgb2gray(img_rgb)
+
+        # 2D register
+        shifts, error, phasediff = register_translation(img_gray, ref_gray)
+
+        # Construct transform object
+        tx = SimilarityTransform(translation=shifts)
+
+        # Apply translation
+        img_rgb_tx = warp(img_rgb, tx)
+
+        # Add to transformed image list
+        stack_rgb_moco.append(img_rgb_tx)
+
+    return stack_rgb_moco
 
 
-def register_stack(stack):
+def blind_deconvolve(stack_rgb):
 
-    return []
+    stack_decon = list()
+
+    for rgb in stack_rgb:
+        pass
+
+    return stack_decon
 
 
-def load_raw_stack(raw_dir):
+def load_png_stack(png_dir):
+    """
+    Load PNG series from directory provided
 
-    return []
+    :param png_dir:
+    :return:
+    """
+
+    # Init empty list for image stack_rgb
+    stack_rgb = list()
+
+    for fname in os.listdir(png_dir):
+
+        try:
+            img = io.imread(fname)
+        except:
+            img = []
+            pass
+
+        if len(img) > 0:
+            print('Loaded %s' % fname)
+            stack_rgb.append(img)
+
+    if len(stack_rgb) < 1:
+        print('No images were loaded')
+
+    return stack_rgb
 
 
-def write_output(out_dir):
+def save_stack(stack_rgb, out_dir):
 
-    return []
+    from skimage.io import imshow, imsave
+
+    for ic, img in enumerate(stack_rgb):
+
+        fname = os.path.join(out_dir, 'img_%03d.png' % ic)
+        imsave(fname, img)
+
+    return
 
 
 # This is the standard boilerplate that calls the main() function.
