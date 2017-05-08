999af036e71e9b4082348dc723a6d192378cd484
Mike Tyszka <jmt@caltech.edu>
2017-05-08 07:52:38 -0700

    OpenCV version. Feature matching for rigid registration

--- stellate.py
+++ stellate.py
@@ -42,8 +42,7 @@ __version__ = '0.1.0'
 import os
 import sys
 import argparse
-import skimage as ski
-from scipy.misc import imresize
+from glob import glob
 
 
 def main():
@@ -55,20 +54,20 @@ def main():
 
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Blind deconvolution and registration of astrophoto stacks')
-    parser.add_argument('-i','--indir', help='Input directory containing RAW image stack')
+    parser.add_argument('-i','--indir', help='Input directory containing PNG image stack')
     parser.add_argument('-o','--outdir', help='Output directory"]')
 
     # Parse command line arguments
     args = parser.parse_args()
 
     if args.indir:
-        raw_dir = args.indir
-        if not os.path.isdir(raw_dir):
-            print('RAW input directory does not exist (%s) - exiting' % raw_dir)
+        png_dir = args.indir
+        if not os.path.isdir(png_dir):
+            print('RAW input directory does not exist (%s) - exiting' % png_dir)
             sys.exit(1)
     else:
         # Default to current directory
-        raw_dir = os.path.realpath(os.getcwd())
+        png_dir = os.path.realpath(os.getcwd())
 
     if args.outdir:
         out_dir = args.outdir
@@ -79,135 +78,75 @@ def main():
     if not os.path.isdir(out_dir):
         os.makedirs(out_dir, exist_ok=True)
 
-    print('Input directory  : %s' % raw_dir)
+    print('Input directory  : %s' % png_dir)
     print('Output directory : %s' % out_dir)
 
-    # Load all RAW images from input directory
-    stack_rgb = load_png_stack(raw_dir)
+    # Get image list from input directory
+    png_list = glob(os.path.join(png_dir,'*.png'))
 
-    # Calculate luminance
-    stack_lum = luminance(stack_rgb)
+    print('Found %d PNGs in %s' % (len(png_list), png_dir))
 
-    # Motion correct frames
-    stack_rgb_moco = moco(stack_lum, stack_rgb)
+    # Load the first image to use as a motion reference
+    img_ref = load_png(png_list[0])
 
-    # Save corrected stack
-    save_stack(stack_rgb_moco, out_dir)
+    for png_fname in png_list:
+
+        print('  Processing %s' % os.path.basename(png_fname))
+
+        # Load the RGB frame from the current PNG file
+        img = load_png(png_fname)
+
+        # Rigid-body motion correct frame
+        img_moco = moco(img, img_ref)
+
+        # Save corrected PNG
+        out_bname = os.path.basename(png_fname)
+        out_fname = os.path.join(out_dir, out_bname.replace('.png', '_corr.png'))
+        save_png(out_fname, img_moco)
 
     # Clean exit
     sys.exit(0)
 
 
-def moco(stack_lum, stack_rgb):
+def moco(img, img_ref):
 
-    import skimage.transform as tf
-    from skimage.feature import register_translation
+    from cv2 import estimateRigidTransform, warpAffine
 
-    # Image dimensions
-    ny, nx = stack_lum[0].shape
-    print('  Frame size (rows x cols) : %d x %d' % (ny, nx))
+    ny, nx, _ = img.shape
 
-    # Moco estimation target image width
-    nx_dwn = 256
-    sf = nx_dwn / float(nx)
+    M = estimateRigidTransform(img, img_ref, fullAffine=False)
 
-    # Prep registration reference image
-    ref_dwn = imresize(stack_lum[0], sf, interp='lanczos')
+    img_moco = warpAffine(img, M, (nx, ny))
 
-    # Init motion corrected stack
-    stack_rgb_moco = list()
-
-    print('  Starting displacement estimation')
-
-    for tc, img in enumerate(stack_lum):
-
-        # Downsample
-        print('  - downsampling (x %0.3f)' % sf)
-        s_dwn = imresize(img, sf, interp='lanczos')
-
-        # Run Fourier cross correlation
-        print('  - estimating displacement')
-        disp, err, pd = register_translation(s_dwn, ref_dwn)
-
-        # Rescale displacements to full resolution
-        dy, dx = disp/sf
-        print('  - estimated displacement (dx, dy) : (%0.1f, %0.1f)' % (dx, dy))
-
-        # Apply motion correction to full resolution RGB image
-        print('  - correcting image')
-        moco = tf.EuclideanTransform(translation=[dx, dy])
-        rgb_moco = tf.warp(stack_rgb[tc], moco)
-
-        # Add to transformed image list
-        stack_rgb_moco.append(rgb_moco)
-
-    return stack_rgb_moco
+    return img_moco
 
 
-def luminance(stack_rgb):
+def load_png(png_fname, sf=0.25):
 
-    stack_gray = list()
+    from cv2 import imread, resize
 
-    for rgb in stack_rgb:
-        stack_gray.append(ski.color.rgb2gray(rgb))
+    try:
+        img = imread(png_fname)
+    except:
+        print('* Problem loading %s' % png_fname)
+        img = []
 
-    return stack_gray
+    # Optional resampling
+    if sf < 1.0:
+        print('  Resizing (sf = %0.2f)' % sf)
+        img = resize(img, (0,0), fx=sf, fy=sf)
 
-
-def blind_deconvolve(stack_rgb):
-
-    stack_decon = list()
-
-    for rgb in stack_rgb:
-        pass
-
-    return stack_decon
+    return img
 
 
-def load_png_stack(png_dir):
-    """
-    Load PNG series from directory provided
+def save_png(png_fname, img):
 
-    :param png_dir:
-    :return:
-    """
+    from cv2 import imwrite
 
-    from skimage.io import imread
-
-    # Init empty list for image stack_rgb
-    stack_rgb = list()
-
-    print('  Loading images from %s' % png_dir)
-
-    for fname in os.listdir(png_dir):
-
-        try:
-            img = imread(fname)
-        except:
-            img = []
-            pass
-
-        if len(img) > 0:
-            print('  Loaded %s' % fname)
-            img = imresize(img, 0.5)
-            stack_rgb.append(img)
-
-    if len(stack_rgb) < 1:
-        print('* No images were loaded')
-
-    return stack_rgb
-
-
-def save_stack(stack_rgb, out_dir):
-
-    for ic, img in enumerate(stack_rgb):
-
-        fname = os.path.join(out_dir, 'img_%03d.png' % ic)
-
-        print('  Saving image to %s' % fname)
-        ski.io.imsave(fname, img)
-
-    return
+    try:
+        imwrite(png_fname, img)
+    except:
+        print('* Problem saving %s' % png_fname)
 
 
 # This is the standard boilerplate that calls the main() function.
